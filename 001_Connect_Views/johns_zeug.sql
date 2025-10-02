d8629388b28089ec2749d30f9c97d1d074a6316c
fuchs-git <pasvoss@gmail.com>
2025-10-02 08:59:15 +0200

    Schema anlegen, löschen

--- 001_Connect_Views/johns_zeug.sql
+++ 001_Connect_Views/johns_zeug.sql
@@ -1,2 +1,7 @@
 
 SELECT rolname, rolinherit, rolsuper, rolcanlogin, rolpassword from pg_authid WHERE rolname not LIKE 'pg_%';
+
+
+SELECT * FROM pg_catalog.pg_namespace INNER JOIN pg_authid ON pg_namespace.nspowner = pg_authid.oid;
+
+SELECT * from pg_authid;
--- 001_Connect_Views/test.sql
+++ 001_Connect_Views/test.sql
@@ -11,3 +11,15 @@ SELECT rolname, rolpassword FROM pg_roles where rolname not like 'pg_%';
 
 
 SELECT rolname, rolinherit, rolsuper, rolcreatedb, rolcanlogin, rolpassword from pg_authid WHERE rolname not LIKE 'pg_%';
+
+SELECT * from pg_authid;
+
+
+SELECT nspname as schema, rolname as besitzer FROM pg_catalog.pg_namespace INNER JOIN pg_authid ON pg_namespace.nspowner = pg_authid.oid;
+
+CREATE SCHEMA dinge;
+create TABLE in_dinge(dings_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY , dings_inhalt text);
+
+drop TABLE in_dinge;
+DROP SCHEMA dinge;
+
